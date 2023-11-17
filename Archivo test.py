f56a9488fec86e5d78e60ca02d3cf1677c4ca94e
Eli2301 <pillita.1023@gmail.com>
2023-11-16 21:33:27 -0600

    Primera version del archivo

---
+++ Archivo test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print ('Bienvenido al curso de Python')
+
+
+# In[4]:
+
+
+print (123)
+
+
+# In[5]:
+
+
+x= 10.5
+y= 20
+
+
+# In[6]:
+
+
+print (x)
+
+
+# In[7]:
+
+
+print ("el valor de x es", x)
+
+
+# In[8]:
+
+
+z = x + y
+print("El resultado de z es", z)
+
+
+# In[10]:
+
+
+long_objeto1 = 10.5
+long_objeto2 = 20
+long_objeto3= long_objeto1 + long_objeto2
+print(long_objeto3)
+
+
+# In[ ]:
+
+
+
+
